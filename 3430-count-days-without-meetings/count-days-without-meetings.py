6011a0f1e4219473ad01180d3f68416ab1f59989
lasyaSiri <lasyasiri12345@gmail.com>
2025-03-24 22:26:36 +0530

    Time: 298 ms (32.00%) | Memory: 47.6 MB (92.00%) - LeetSync

---
+++ 3430-count-days-without-meetings/count-days-without-meetings.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def countDays(self, days, meetings):
+        meetings.sort()
+        prevEnd = 0
+
+        for start, end in meetings:
+            start = max(start, prevEnd + 1)
+            length = end - start + 1
+            days -= max(length, 0)
+            prevEnd = max(prevEnd, end)
+
+        return days
+        
